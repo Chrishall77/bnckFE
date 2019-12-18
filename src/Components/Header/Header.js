d69aa6025ce03e6d964675017545d684f1797b79
papercranecode <papercranecode@users.noreply.github.com>
2019-12-18 12:51:43 +0000

    Created a new component Header.

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+    <>
+        <div className="banner">
+            <div className="navLink"> { '' } </div>
+            <h1 class="bannerTitle"> { '' } </h1>
+            <div class="navBurger"> { '' } </div>
+        </div>
+    </>
+);
+
+export default Header;
