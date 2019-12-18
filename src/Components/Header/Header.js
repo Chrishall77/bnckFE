ff0c082cc053f709f2bd4f033ab63068c2b4335f
papercranecode <papercranecode@users.noreply.github.com>
2019-12-18 13:07:19 +0000

    Added in the potato and burger.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import potato from '../../styling/assets/potato.svg';
+import burger from '../../styling/assets/burger.svg';
 
 const Header = () => (
     <>
         <div className="banner">
-            <div className="navLink"> { '' } </div>
-            <h1 class="bannerTitle"> { '' } </h1>
-            <div class="navBurger"> { '' } </div>
+            <div className="navLink"> <img src={potato} /> </div>
+            <h1 class="bannerTitle"> { /*PAGE TITLE*/ } </h1>
+            <div class="navBurger"> <img src={burger} /> </div>
         </div>
     </>
 );
