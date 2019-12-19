109061b8444ba85bc7ecc7cec0716b6ddd4eb903
Bethany Everett <bethanyeeverett@gmail.com>
2019-12-19 17:02:13 +0000

    Formatting time in start/end time performances.

--- src/Components/Performances/Performances.js
+++ src/Components/Performances/Performances.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { moment } from "moment";
 
 
 
@@ -26,15 +27,25 @@ export default class Performances extends React.Component {
                                 return artist.id === performance.artist_id;
                             })
         
+                            let formatDate = (date) => {
+
+                                return date.toLocaleTimeString();
+
+                            }
+
+                            let starttime = new Date(performance.starttime);
+                            let endtime = new Date(performance.starttime);
+                            
+
                             return (
         
                                 <li className="artist" key={ performance.id }>
                                     <div className="buttonStyleFour timeButtonTime">
-                                        <p>Start: { performance.starttime } </p>
-                                        <p>End: { performance.endtime } </p>
+                                        <p>Start: { formatDate(starttime) } </p>
+                                        <p>End: { formatDate(endtime) } </p>
                                     </div>
                                     <div className="buttonStyleFive timeButtonArtist">
-                                        {/* <p>{ titleStage ? "Stage: " + stageName.name : "Artist: " + artistName.name }</p> */}
+                                        <p>{ titleStage ? "Stage: " + stageName.name : "Artist: " + artistName.name }</p>
                                     </div>
                                 </li>
                             )
