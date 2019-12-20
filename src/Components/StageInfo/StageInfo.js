1a3ceb813d2f17c92c898bb12bb730bf418c95ed
papercranecode <papercranecode@users.noreply.github.com>
2019-12-20 11:17:36 +0000

    Updated styling.

--- src/Components/StageInfo/StageInfo.js
+++ src/Components/StageInfo/StageInfo.js
@@ -12,7 +12,7 @@ const StageInfo = ({ location }) => {
     return (
         <>
             <Header>NAME</Header>
-            <div className="stageInfoBackground stageInfoBackgroundPosition">
+            <div className={`stage${id}`}>
                 <Performances 
                     referrer={ referrer }
                     id={ id }
