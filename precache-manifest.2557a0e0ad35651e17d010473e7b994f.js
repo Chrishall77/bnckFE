e6a048c5e1eef8548c088811f4272061b6dbf617
Chris Hall <chrishallhome@gmail.com>
2019-12-20 15:49:00 +0000

    Updates

---
+++ precache-manifest.2557a0e0ad35651e17d010473e7b994f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531f838c24099b104859eb3b37e6edef",
+    "url": "/bnckFE/index.html"
+  },
+  {
+    "revision": "25ec26dcb7e2f73b7088",
+    "url": "/bnckFE/static/css/main.2aa54bf1.chunk.css"
+  },
+  {
+    "revision": "f9b8a69e1c624658a8b0",
+    "url": "/bnckFE/static/js/2.5280e4c8.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/bnckFE/static/js/2.5280e4c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25ec26dcb7e2f73b7088",
+    "url": "/bnckFE/static/js/main.638e70ff.chunk.js"
+  },
+  {
+    "revision": "c72d4991d21a92358a36",
+    "url": "/bnckFE/static/js/runtime-main.a30bac41.js"
+  },
+  {
+    "revision": "555251f3e52724007d4046fb4d21a8a9",
+    "url": "/bnckFE/static/media/artistsBackground2.555251f3.jpg"
+  },
+  {
+    "revision": "eabb3d6a10151d58ab1f65f388fbbabb",
+    "url": "/bnckFE/static/media/bigLoggerBackground2.eabb3d6a.jpg"
+  },
+  {
+    "revision": "4dcfadcc2091fdf2e81edad27db0d125",
+    "url": "/bnckFE/static/media/burger.4dcfadcc.svg"
+  },
+  {
+    "revision": "fd4896afdda3f8e7f8a54618b91deccb",
+    "url": "/bnckFE/static/media/cathedralBackground2.fd4896af.jpg"
+  },
+  {
+    "revision": "fe90e4f8b2722d274b942329fe18058f",
+    "url": "/bnckFE/static/media/duttyDiscoBackground2.fe90e4f8.jpg"
+  },
+  {
+    "revision": "cd7a7caf4156a9230d27b33d67755dd3",
+    "url": "/bnckFE/static/media/landingBackground2.cd7a7caf.jpg"
+  },
+  {
+    "revision": "48fc3170d3b8f116d44da4a4b66eaa9d",
+    "url": "/bnckFE/static/media/performancesBackground2.48fc3170.jpg"
+  },
+  {
+    "revision": "54cdb15dd6222f71e9bc2a753639908d",
+    "url": "/bnckFE/static/media/potato.54cdb15d.svg"
+  },
+  {
+    "revision": "c1bc2a552d82db4e4ee8a566bb9bab1b",
+    "url": "/bnckFE/static/media/smoothMooversBackground2.c1bc2a55.jpg"
+  },
+  {
+    "revision": "2c1340f12fa0e5430368be797ff230e1",
+    "url": "/bnckFE/static/media/stagesBackground2.2c1340f1.jpg"
+  }
+]);
