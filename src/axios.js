a6c2fee2c41fd1c44558cdab7a858a33c1234251
Chris Hall <chrishallhome@gmail.com>
2019-12-20 15:42:51 +0000

    Updated to AWS API

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,6 @@
 import axios from "axios";
 // return a new version of axios with useful settings applied
 export default axios.create({
-baseURL: "http://bnckfest.test/api", headers: {
+baseURL: "https://bnck.developme.space/api/", headers: {
         Accept: "application/json",
 }, });
